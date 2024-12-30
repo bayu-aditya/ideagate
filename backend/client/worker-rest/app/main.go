7c77e426484f19aecd0a69f98736f6f1f845448f
Bayu Aditya <itachibayu@gmail.com>
2024-12-30 14:40:00 +0700

    fix log

--- backend/client/worker-rest/app/main.go
+++ backend/client/worker-rest/app/main.go
@@ -15,20 +15,20 @@ func main() {
 	router := gin.Default()
 
 	if err := config.Init(); err != nil {
-		log.Fatal("init config", err)
+		log.Fatal("init config: %v", err)
 	}
 
 	adapterController, err := controller.New()
 	if err != nil {
-		log.Fatal("init controller adapter", err)
+		log.Fatal("init controller adapter: %v", err)
 	}
 
 	usecaseHandler := handler.New(adapterController)
 	if err := usecaseHandler.GenerateEndpoint(ctx, router); err != nil {
-		log.Fatal("generate endpoint", err)
+		log.Fatal("generate endpoint: %v", err)
 	}
 
 	if err := router.Run(); err != nil {
-		log.Fatal("run router", err)
+		log.Fatal("run router: %v", err)
 	}
 }
